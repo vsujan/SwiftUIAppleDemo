484529b5441a831fbf175889563ae2058b6ed5d0
Sujan Vaidya <shujan1992@gmail.com>
2019-12-31 16:37:54 +0545

    Creating and combining views

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Sujan Vaidya on 12/31/19.
+//  Copyright © 2019 Sujan Vaidya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
