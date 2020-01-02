8c8bc5b5268cc1721a1b4be61024a3a83e7e84dc
Sujan Vaidya <shujan1992@gmail.com>
2020-01-02 13:39:32 +0545

    Composing complex interfaces

--- SwiftUIDemo/LandmarkList.swift
+++ SwiftUIDemo/LandmarkList.swift
@@ -13,31 +13,25 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Favorites only")
-                }
-
-                ForEach(self.userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Favorites only")
+            }
+            
+            ForEach(self.userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle(Text("Landmarks"))
         }
+        .navigationBarTitle(Text("Landmarks"))
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-//        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-//                .previewDevice(PreviewDevice(rawValue: deviceName))
-//                .previewDisplayName(deviceName)
-//        }
+        LandmarkList()
     }
 }
